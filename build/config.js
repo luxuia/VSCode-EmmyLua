d6a5773d1f899b81bafb48b9d61f2b6a6efebe8d
luxuia <luxuia@foxmail.com>
2024-07-07 23:57:20 +0800

    'xx'

--- build/config.js
+++ build/config.js
@@ -2,5 +2,5 @@ exports.default = {
     emmyDebuggerVersion: '1.8.2',
     emmyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaDebugger/releases/download',
     newLanguageServerVersion: "0.4.0",
-    newLanguageServerUrl: "https://github.com/CppCXY/EmmyLuaAnalyzer/releases/download"
+    newLanguageServerUrl: "https://github.com/luxuia/EmmyLuaAnalyzer/releases/download"
 }
